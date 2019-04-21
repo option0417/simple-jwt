f1aec893ff6c11cc0603e61c4b815db3fdb9fbdb
option0417 <option0417@gmail.com>
2019-04-22 02:18:26 +0800

    Update

---
+++ src/main/java/tw/com/wd/jwt/Executor.java
@@ -0,0 +1,13 @@
+package tw.com.wd.jwt;
+
+/**
+ * Hello world!
+ *
+ */
+public class Executor {
+    public static void main( String[] args ) {
+        JsonWebTokenService jsonWebTokenService = new JsonWebTokenService();
+
+        System.out.println( "Hello World!" );
+    }
+}
